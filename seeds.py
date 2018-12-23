8691f3591caf6c33b27a36e33dd92eab07abda4d
James Robins <jamesrobins101@hotmail.com>
2018-12-23 00:13:58 +0000

    removed example cocktail from seeds file

--- seeds.py
+++ seeds.py
@@ -1,8 +1,5 @@
 from app import app, db
 from models.Cocktail import Cocktail
-# from models.User import User
-# from models.Ingredient import Ingredient
-# from flask import jsonify
 import requests
 
 
@@ -10,33 +7,10 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-    # all_ingredients = requests.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').json()
-    # # print(all_ingredients['drinks'])
-    #
-    # for each_ingredient in all_ingredients['drinks']:
-    #     ingredient_name = each_ingredient['strIngredient1'].replace(" ", "_")
-    #     print(ingredient_name)
-    #     ingredient_name = Ingredient({
-    #         'name': each_ingredient['strIngredient1']
-    #     })
-    #     ingredient_name.save()
-
-    blueberry_smash = Cocktail({
-        'name': 'Blueberry Smash',
-        'method': 'Fill a glass with ice. Add the blueberry thyme syrup, followed by the Gin, and top with the lemon-lime soda. Stir gently to combine, then garnish with fresh blueberries, thyme sprigs, and a slice of fresh lime. Enjoy responsibly.',
-        'image': 'https://i.postimg.cc/j5vBYfj7/Easy-Cocktail-Recipes-Blueberry-Thyme-Smash-7.jpg',
-    })
-    ingredients = [{"amount": "2 Tbsp", "name": "Blueberry thyme syrup"}, {"amount": "2 oz", "name": "Gin"}, {"amount": "5 oz", "name": "Sprite"}]
-
-    blueberry_smash.add_ingredients(ingredients)
-
-    blueberry_smash.save()
-
     # TEST SEEDS:
     # all_cocktails = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Egg').json()
 
     all_cocktails = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic').json()
-    # print(all_cocktails['drinks'])
 
     for each_cocktail in all_cocktails['drinks']:
         print(each_cocktail['strDrink'] + ' ' + each_cocktail['idDrink'])
@@ -54,7 +28,6 @@ with app.app_context():
 
         i = 1
         while i < 15:
-            # print(one_cocktail['drinks'][0]['strIngredient{}'.format(i)])
 
             if (one_cocktail['drinks'][0]['strIngredient{}'.format(i)] != '' and one_cocktail['drinks'][0]['strIngredient{}'.format(i)] is not None):
 
